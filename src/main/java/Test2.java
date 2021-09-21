0727b31fe3fef7e15209a343d602dfede1aa136f
whwstart <whwstart0314@163.com>
2021-09-21 19:36:48 +0800

    ..

---
+++ src/main/java/Test2.java
@@ -0,0 +1,12 @@
+/**
+ * @author whwstrat
+ * @createTime 2021-09-21 19:35
+ * @DESC
+ */
+public class Test2 {
+
+
+    public static void main(String[] args) {
+        System.out.println("zaishiyixia");
+    }
+}
