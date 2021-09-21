8bde4c39d88d966bd1cb99e9ef1c3df86c3e5253
whwstart <whwstart0314@163.com>
2021-09-21 19:35:06 +0800

    Initial commit

---
+++ src/main/java/TestGit.java
@@ -0,0 +1,12 @@
+/**
+ * @author whwstrat
+ * @createTime 2021-09-21 19:34
+ * @DESC
+ */
+public class TestGit {
+    public static void main(String[] args) {
+
+        System.out.println("shiyixia");
+
+    }
+}
